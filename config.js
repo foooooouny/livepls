5122d98c10b5601b7f988e5d5e151fb6791da751
bpceee <bipengcheng@gmail.com>
2016-05-05 19:31:44 +0800

    production

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
   actilive: {
-    apiHost: 'http://127.0.0.1:8000',
+    // apiHost: 'http://127.0.0.1:8000',
+    apiHost: 'http://live.videojj.com',
     secret: 'VkxJrMr--b',
     platformId: '556c38e7ec69d5bf655a0fb2',
   },
