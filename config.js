9102c7b572a9cd5097cccd70ad7a783624497af0
bpceee <bipengcheng@gmail.com>
2016-05-05 19:31:44 +0800

    production

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
   actilive: {
-    apiHost: 'http://127.0.0.1:8000',
-    secret: 'VkxJrMr--b',
+    // apiHost: 'http://127.0.0.1:8000',
+    apiHost: 'http://live.videojj.com',
+    secret: '4JdmWKQGW',
     platformId: '556c38e7ec69d5bf655a0fb2',
   },
 }
