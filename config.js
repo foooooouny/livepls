a63212d51c4ff48205069acb5a391fc26ccdd838
李鹏程 <379623292@qq.com>
2016-09-23 18:37:02 +0800

    style: 更新阿里oss 地址到 cdn加速

--- config.js
+++ config.js
@@ -4,13 +4,13 @@ module.exports = {
     // apiHost: 'http://liveapi.videojj.com',
     secret: '4JdmWKQGW',
     platformId: '556c38e7ec69d5bf655a0fb2',
-    cdnJS: 'http://videojj-cdn.oss-cn-beijing.aliyuncs.com/actilive/dev/actilive.js',
-    cdnCSS: 'http://videojj-cdn.oss-cn-beijing.aliyuncs.com/actilive/dev/actilive.css'
+    cdnJS: 'http://sdk.cdn.videojj.com/actilive/dev/actilive.js',
+    cdnCSS: 'http://sdk.cdn.videojj.com/actilive/dev/actilive.css'
   },
   prod: {
     secret: 'SJ_d6e6i',
     platformId: '575e6e087c395e0501980c89',
-    cdnJS: 'http://videojj-cdn.oss-cn-beijing.aliyuncs.com/actilive/actilive.js',
-    cdnCSS: 'http://videojj-cdn.oss-cn-beijing.aliyuncs.com/actilive/actilive.css'
+    cdnJS: 'http://sdk.cdn.videojj.com/actilive/actilive.js',
+    cdnCSS: 'http://sdk.cdn.videojj.com/actilive/actilive.css'
   }
 }
