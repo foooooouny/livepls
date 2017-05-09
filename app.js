e826e4610a209d8b2bf6153bd80b1f182a8b3647
李鹏程 <379623292@qq.com>
2017-05-09 16:45:49 +0800

    feat: 热更新

--- app.js
+++ app.js
@@ -319,7 +319,6 @@ app.get('/user/my/:id', function (req, res) {
                 } else {
                     renderData.hudong = 'active'
                 }
-                renderData.t = Date.now()
                 res.render('information', renderData);
             })
         });
--- config.js
+++ config.js
@@ -4,16 +4,16 @@ module.exports = {
     // apiHost: 'http://liveapi.videojj.com',
     secret: '4JdmWKQGW',
     platformId: '556c38e7ec69d5bf655a0fb2',
-    cdnJS: 'http://sdk.cdn.videojj.com/actilive/dev/actilive.js',
-    cdnCSS: 'http://sdk.cdn.videojj.com/actilive/dev/actilive.css',
+    cdnJS: `http://sdk.cdn.videojj.com/actilive/dev/actilive.js?t=${Date.now()}`,
+    cdnCSS: `http://sdk.cdn.videojj.com/actilive/dev/actilive.css?t=${Date.now()}`,
     flashUI: 'http://sdk.cdn.videojj.com/flash/player/player_ui.swf',
     flashApi: 'http://sdk.cdn.videojj.com/flash/player/VideoJJ.swf'
   },
   prod: {
     secret: 'SJ_d6e6i',
     platformId: '575e6e087c395e0501980c89',
-    cdnJS: 'https://sdkcdn.videojj.com/actilive/actilive.js',
-    cdnCSS: 'https://sdkcdn.videojj.com/actilive/actilive.css',
+    cdnJS: `https://sdkcdn.videojj.com/actilive/actilive.js?t=${Date.now()}`,
+    cdnCSS: `https://sdkcdn.videojj.com/actilive/actilive.css?t=${Date.now()}`,
     flashUI: 'https://sdkcdn.videojj.com/flash/player/player_ui.swf',
     flashApi: 'https://sdkcdn.videojj.com/flash/player/bin/VideoJJ.swf'
   }
